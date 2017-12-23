41694fbb16f7bf5dbe735f0cd2fd2c2ceb28e0bd
Tony0208 <1289610740@qq.com>
2017-12-23 20:54:32 +0800

    图片转换为字符

---
+++ spider/PictureConversionCharacter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+-------------------------------------------------
+   File Name:    PictureConversionCharacter
+   Description : 图片转换为字符
+   Author :       tony
+   date:         2017/12/23 20:53
+-------------------------------------------------
+'''
